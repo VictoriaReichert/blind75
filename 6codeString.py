225992f5aa4c6171d6343ea52b710332d80331b8
VictoriaReichert <vika.raihert@gmail.com>
2026-02-12 21:51:47 +0300

    6 задача, не лучшее решение

--- 6codeString.py
+++ 6codeString.py
@@ -1,1 +1,17 @@
 # 6 задача
+from typing import List
+
+
+def encode(strs: List[str]) -> str:
+    newStr = ''
+    newStr = ';'.join(strs)
+    #print(newStr)
+
+    if len(strs) == 0:
+        return []
+    oldStr = ''
+    oldStr = newStr.split(';')
+    return oldStr
+
+
+print(encode([]))
