4642e0514a16cfd7d48c1ec78c8a2b33b1ddef25
VictoriaReichert <vika.raihert@gmail.com>
2026-02-14 16:49:25 +0300

    7 задача решена, осталось выучить про префикс

---
+++ 7arrayMult.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    numsMult = 1
+    numsMultZero = 1
+    for n in nums:
+        numsMult *= n
+        if n != 0:
+            numsMultZero *= n
+
+    output = []
+    for n in nums:
+        if numsMult == 0 and n != 0:
+            output.append(0)
+        elif numsMult == 0 and n == 0:
+            output.append(numsMultZero)
+        elif numsMult != 0:
+            output.append(int(numsMult/n))
+    return output
+
+
+def multExcludeI(nums: List[int], i: int) -> int:
+    numMult = 1
+    for j in range(len(nums)):
+        if j != i:
+            numMult *= nums[j]
+    return numMult
+
+
+def productExceptSelf2(nums: List[int]) -> List[int]:
+    output = []
+    for i in range(len(nums)):
+        output.append(multExcludeI(nums, i))
+    return output
+
+
+def productExceptSelf3(nums: List[int]) -> List[int]:
+    n = len(nums)
+    res = []
+    pref = []
+    suff = []
+    for i in range(n):
+        suffixArray[i].append(i+1)
+    return output
+
+
+print(productExceptSelf2([-1,0,1,2,3]))
